d4ddb4ffa387a84c736431021b2b24ac4eac1364
Nerakma84 <fnk8476@gmail.com>
2022-09-12 20:29:26 -0500

    i have to

--- assets/script.js
+++ assets/script.js
@@ -4,11 +4,11 @@ function getAPIRecipe () {
 		method: 'GET',
 		headers: {
 			'X-RapidAPI-Key': '49bd2d9a87msh820cee85c9b73abp1a58c4jsnafb238f3371d',
-			'X-RapidAPI-Host': 'edamam-recipe-search.p.rapidapi.com'
+			'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
 		}
 	};
 	
-	fetch('https://edamam-recipe-search.p.rapidapi.com/search?q=chicken', options)
+	fetch('https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=chicken%20soup', options)
 		.then(response => response.json())
 		.then(response => console.log(response))
 		.catch(err => console.error(err));
@@ -17,24 +17,22 @@ function getAPIRecipe () {
 
 function getAPITranslate () {
 	const options = {
-	method: 'POST',
-	headers: {
-		'content-type': 'application/x-www-form-urlencoded',
-		'Accept-Encoding': 'application/gzip',
-		'X-RapidAPI-Key': '49bd2d9a87msh820cee85c9b73abp1a58c4jsnafb238f3371d',
-		'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
-	},
-	body: encodedParams
+		method: 'POST',
+		headers: {
+			'content-type': 'application/json',
+			'X-RapidAPI-Key': '1f4028efdbmshbe7a88de4a624b0p138246jsnde0a0e24dee3',
+			'X-RapidAPI-Host': 'deepl-translator.p.rapidapi.com'
+		},
+		body: '{"text":"Language selection.","source":"en","target":"es"}'
+	};
+	
+	fetch('https://deepl-translator.p.rapidapi.com/translate/', options)
+		.then(response => response.json())
+		.then(response => console.log(response))
+		.catch(err => console.error(err))
 
-//  Look up encodedParams above
 };
 
-fetch('https://google-translate1.p.rapidapi.com/language/translate/v2/detect', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-
-}
 
 
 getAPIRecipe();
